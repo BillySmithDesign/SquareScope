576e062d0990f465a353febc07b76c5623c6e1c7
Billy <billysmithmelb@gmail.com>
2026-09-14 16:33:45 +0930

    Mount mobile analytics enhancement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import MobileAnalyticsEnhancer from "./mobile-analytics-enhancer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -51,7 +52,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <MobileAnalyticsEnhancer />
+      </body>
     </html>
   );
 }
